f3d102b8fe441e3ba0393a80d4ebc81a83567080
bechollet <benjamin.chollet@bordeaux-inp.fr>
2021-02-09 11:52:19 +0100

    Etape_5_Ok

--- 1_Software/Etape_5/src/main.c
+++ 1_Software/Etape_5/src/main.c
@@ -2,6 +2,7 @@
 #include "stdlib.h"
 #include "math.h"
 #include "time.h"
+#include <assert.h>
 
 int RandA()
 {
@@ -17,32 +18,31 @@ int PGCD(int A, int B)
 	assert(A < 65536);
 	assert(B < 65536);
 	assert(A >= 0);
-	assert(B >=0);
+	assert(B >= 0);
 
+	int Entree_A = A;
+	int Entree_B = B;
 	if (A == 0 && B == 0) return 0;
-	if (B == 0) return A;
-	if (A == 0) return B;
+	else if (B == 0) return A;
+	else if (A == 0) return B;
 
 	while(A!=B){
 		if (A > B) A = A-B;
 		else B = B-A;
 	}
+	assert(A >= 0);
+	assert(A < Entree_A);
+	assert(A < Entree_B);
+	assert((Entree_A %A) == 0);
+	assert((Entree_B %A) == 0);
 	return A;
 }
 
-
-//On tombe sur une valeur nulle, donc le programme plante
 int main (int argc, char * argv []){
 	printf("(II) Starting PGCD program\n");
-	int A = 0;
-	int B = 0;
-	srand( time( NULL ) );
 
-	for (int i = 0; i <200000; ++i) {
-		A = RandA();
-		B = RandB();
-		printf("(%d) : PGCD(%d;%d) = %d\n",i,A,B,PGCD(A,B));
-	}
+	PGCD(10,5);
+
 	printf("(II) End of PGCD program\n");
   return 0;
 }
