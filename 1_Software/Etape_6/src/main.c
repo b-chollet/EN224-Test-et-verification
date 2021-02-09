30a2892237202f0197142d40325e4a03097ed7cf
bechollet <benjamin.chollet@bordeaux-inp.fr>
2021-02-09 12:05:07 +0100

    Etape_6_Ok

--- 1_Software/Etape_6/src/main.c
+++ 1_Software/Etape_6/src/main.c
@@ -41,7 +41,12 @@ int PGCD(int A, int B)
 int main (int argc, char * argv []){
 	printf("(II) Starting PGCD program\n");
 
-	PGCD(10,5);
+	assert(PGCD(0,0) == 0);
+	assert(PGCD(10,0) == 10);
+	assert(PGCD(0,56) == 56);
+	assert(PGCD(65535,65535) == 65535);
+	assert(PGCD(65535,10000) == 5);
+	assert(PGCD(10000,65535) == 5);
 
 	printf("(II) End of PGCD program\n");
   return 0;
