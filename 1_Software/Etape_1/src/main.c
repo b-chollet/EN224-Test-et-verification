5d89aacdbf8e5a19ccc0b153841be899cb52a287
bechollet <benjamin.chollet@bordeaux-inp.fr>
2021-02-09 09:07:16 +0100

    Etape_1_Ok

--- 1_Software/Etape_1/src/main.c
+++ 1_Software/Etape_1/src/main.c
@@ -4,12 +4,23 @@
 
 int PGCD(int A, int B)
 {
-
+	while(A!=B){
+		if (A > B) A = A-B;
+		else B = B-A;
+	}
 	return A;
 }
 
 int main (int argc, char * argv []){
 	printf("(II) Starting PGCD program\n");
+
+	printf("PGCD = %d",PGCD(12,52));
+	printf("PGCD = %d",PGCD(120,22));
+	printf("PGCD = %d",PGCD(90,41));
+	printf("PGCD = %d",PGCD(31,907));
+	printf("PGCD = %d",PGCD(531,578));
+	printf("PGCD = %d",PGCD(15,42));
+	printf("PGCD = %d",PGCD(1,523));
 
 
 	printf("(II) End of PGCD program\n");
