01110fbe11409ec870a7eb1fe08b586376833732
bechollet <benjamin.chollet@bordeaux-inp.fr>
2021-02-09 11:09:42 +0100

    j'en ai marre

--- 1_Software/Etape_4/src/main.c
+++ 1_Software/Etape_4/src/main.c
@@ -19,6 +19,10 @@ int PGCD(int A, int B)
 	assert(A >= 0);
 	assert(B >=0);
 
+	if (A == 0 && B == 0) return 0;
+	if (B == 0) return A;
+	if (A == 0) return B;
+	
 	while(A!=B){
 		if (A > B) A = A-B;
 		else B = B-A;
--- 1_Software/Etape_5/src/main.c
+++ 1_Software/Etape_5/src/main.c
@@ -1,7 +1,16 @@
 #include "stdio.h"
 #include "stdlib.h"
 #include "math.h"
-#include <assert.h>
+#include "time.h"
+
+int RandA()
+{
+	return rand()%65536;
+}
+int RandB()
+{
+	return rand()%65536;
+}
 
 int PGCD(int A, int B)
 {
@@ -10,6 +19,10 @@ int PGCD(int A, int B)
 	assert(A >= 0);
 	assert(B >=0);
 
+	if (A == 0 && B == 0) return 0;
+	if (B == 0) return A;
+	if (A == 0) return B;
+
 	while(A!=B){
 		if (A > B) A = A-B;
 		else B = B-A;
@@ -17,21 +30,19 @@ int PGCD(int A, int B)
 	return A;
 }
 
+
+//On tombe sur une valeur nulle, donc le programme plante
 int main (int argc, char * argv []){
 	printf("(II) Starting PGCD program\n");
+	int A = 0;
+	int B = 0;
+	srand( time( NULL ) );
 
-/*
-	printf("PGCD = %d",PGCD(12,52));
-	printf("PGCD = %d",PGCD(120,22));
-	printf("PGCD = %d",PGCD(90,41));
-	printf("PGCD = %d",PGCD(31,907));
-	printf("PGCD = %d",PGCD(531,578));
-	printf("PGCD = %d",PGCD(15,42));
-	printf("PGCD = %d",PGCD(1,523));
-*/
-	assert(PGCD(12,52) == 4);
-	assert(PGCD(12,52) == 5);
-
+	for (int i = 0; i <200000; ++i) {
+		A = RandA();
+		B = RandB();
+		printf("(%d) : PGCD(%d;%d) = %d\n",i,A,B,PGCD(A,B));
+	}
 	printf("(II) End of PGCD program\n");
   return 0;
 }
